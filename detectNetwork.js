85f2c3bc9dab681fe5ba73e42e8dd4e80b5a7547
Jeffrey Wang <jqywang.95@gmail.com>
2017-12-08 13:27:32 -0800

    "Changed some variables to scale better with evolving problem. Stopped using console.logs."

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,14 +8,39 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-	var isFourteen = (cardNumber.length===14);
-	var isFifteen = (cardNumber.length===15);
+	
+  if (cardNumber === null || cardNumber === undefined){
+  	return "null";
+  	}
+	var firstTwo = cardNumber.slice(0,2);
+	var cardLength = cardNumber.length;
+	var firstOne = cardNumber.slice(0,1);
+
+  if ((firstTwo === "38" || firstTwo === "39") && cardLength === 14) {
+  	return "Diner\'s Club";
+  }
+  if ((firstTwo === "34" || firstTwo === "37") && cardLength === 15) {
+  	return "American Express";
+  }
+  if((parseInt(firstTwo)<56 && parseInt(firstTwo)>50) && cardLength===16){
+  	return "MasterCard"
+  }
+
+	/*var numberLength = cardNumber.length;
 	var firstTwoDigits= cardNumber.slice(0,2);
-	if(isFourteen===true && (firstTwoDigits==='38' || firstTwoDigits==='39')){
+	if(numberLength===14 && (firstTwoDigits==='38' || firstTwoDigits==='39')){
 		console.log('Diner\'s Club');
-	} else if (isFifteen===true &&(firstTwoDigits==='34'||firstTwoDigits==='37')){
+		return ('Diner\'s Club');
+	} 
+	if (numberLength===15 && (firstTwoDigits==='34' || firstTwoDigits==='37')){
 		console.log("American Express");
-	}
+	} 
+
+
+	else if (firstTwoDigits[0]==='4' &&(numberLength===13||numberLength===16||numberLength===19)){
+		console.log("Visa");
+	} 
+	*/
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
