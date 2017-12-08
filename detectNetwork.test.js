c40c3e0a3d2cec5edfb672953ce27a6c918b4362
Jeffrey Wang <jqywang.95@gmail.com>
2017-12-08 13:28:17 -0800

    "changed and debugged testing suite for erroneous tests"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -18,13 +18,12 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // You will not be able to proceed with a failing test. 
 
   it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
   });
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
     // This test doesn't really test anything at all! It will pass no matter what.
     var even = function(num){
-      return num/2 === 0;
+      return num%2 === 0;
     }
     return even(10) === true;
   });
@@ -33,7 +32,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A test should only fail if the expected behavior doesn't match the actual.
   it('Throws an error when expected behavior does not match actual behavior', function() {
     var even = function(num){
-      return num/2 === 0;
+      return num%2 === 0;
     }
 
     if(even(10) !== true) {
@@ -45,15 +44,18 @@ describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
  
-    if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
+    if (detectNetwork('38345678901234') === "Diner\'s Club") {
+      
+    } else {
       throw new Error('Test failed');
     }
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901234') === "Diner\'s Club") {
+      
+    } else {
       throw new Error('Test failed');
     }
  
@@ -84,7 +86,7 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it. 
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+  var assert = chai.assert;
  
 
   it('has a prefix of 4 and a length of 13', function() {
